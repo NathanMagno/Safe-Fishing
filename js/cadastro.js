97109ad46f0226a267306214013e82688df1ec3b
nathan <nathan.consolo17@gmail.com>
2024-05-28 19:02:45 -0300

    Dialog e js implementados

---
+++ js/cadastro.js
@@ -0,0 +1,8 @@
+const esconde = document.querySelector("dialog");
+
+const btnEsconde = document.querySelector("#entrar-modal");
+
+btnEsconde.addEventListener("click",()=>{
+ 
+    esconde.showModal();
+});
