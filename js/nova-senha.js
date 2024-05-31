abf427a227c0bec6332d45156d6917d1429465ba
nathan <nathan.consolo17@gmail.com>
2024-05-31 15:21:50 -0300

    Telas de cadastro finalizadas
    OBS: por enquanto, há alguns pontos para se melhorar no js

--- js/nova-senha.js
+++ js/nova-senha.js
@@ -2,13 +2,12 @@ const btn_salvar = document.querySelector("#btn-salvar-senha");
 
 console.log("nao");
 
-function salve(senha){
+const salve = (senha)=> {
 
-    const senha_usuario = senha.value;
-    console.log(senha.value);
-    localStorage.setItem("userSenha", JSON.stringify(senha_usuario))
-    console.log("foi o btn");
-    setTimeout(window.location.href = "../html/login.html" ,2000);
+    const novaSenha = senha.value;
+    
+    localStorage.setItem("senhaUser", JSON.stringify(novaSenha));
+    window.location.href = "../html/login.html";
 
     return false;
 }
