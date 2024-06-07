6382a315e0de8eee6d342a7078ac4ae1130184f4
nathan <nathan.consolo17@gmail.com>
2024-06-07 10:03:14 -0300

    Validando dados

--- js/criar-conta.js
+++ js/criar-conta.js
@@ -4,10 +4,20 @@ let criarUser =  {nome: "", emailUsuario: "", senhaUsuario: "", cpf: 0 } ;
 
 const criarConta = (nomeU, cpfU, emailU, senhaU) => {
 
-  criarUser.nome = nomeU.value;
-  criarUser.cpf = cpfU.value;
-  criarUser.emailUsuario = emailU.value;
-  criarUser.senhaUsuario = senhaU.value;
+  if (!nomeU.value || !cpfU.value || !emailU.value || !senhaU.value) {
+    alert("Por favor, preencha todos os campos.");
+    return false;
+  }
+  else
+  {
+
+    criarUser.nome = nomeU.value;
+    criarUser.cpf = cpfU.value;
+    criarUser.emailUsuario = emailU.value;
+    criarUser.senhaUsuario = senhaU.value;
+
+    return false;
+  }
 
   localStorage.setItem("novaConta", JSON.stringify(criarUser));
   window.location.href = "../html/login.html";
