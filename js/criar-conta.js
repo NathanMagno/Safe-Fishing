7f90cb329b18bab9856553a53d8b827b60ee2af6
nathan <nathan.consolo17@gmail.com>
2024-06-07 11:41:35 -0300

    Melhorando validacao do criar conta

--- js/criar-conta.js
+++ js/criar-conta.js
@@ -1,29 +1,27 @@
-
-let criarUser =  {nome: "", emailUsuario: "", senhaUsuario: "", cpf: 0 } ;
-
+let criarUser = { nome: "", emailUsuario: "", senhaUsuario: "", cpf: 0 };
 
 const criarConta = (nomeU, cpfU, emailU, senhaU) => {
+  let msg = document.querySelector(".valida-cc");
 
   if (!nomeU.value || !cpfU.value || !emailU.value || !senhaU.value) {
-    alert("Por favor, preencha todos os campos.");
-    return false;
-  }
-  else
-  {
+    msg.setAttribute("class", "negado");
+    msg.textContent = "Por favor, preencha todos os campos.";
 
-    criarUser.nome = nomeU.value;
-    criarUser.cpf = cpfU.value;
-    criarUser.emailUsuario = emailU.value;
-    criarUser.senhaUsuario = senhaU.value;
+    setTimeout(() => {
+      msg.setAttribute("class", "valida-cc");
+      msg.textContent = "";
+    }, 5000);
 
     return false;
   }
+
+  criarUser.nome = nomeU.value;
+  criarUser.cpf = cpfU.value;
+  criarUser.emailUsuario = emailU.value;
+  criarUser.senhaUsuario = senhaU.value;
 
   localStorage.setItem("novaConta", JSON.stringify(criarUser));
   window.location.href = "../html/login.html";
 
-
-
   return false;
-}
-
+};
