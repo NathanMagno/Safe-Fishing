003e5e68826f6c3c904919954e35ff4e77faf70d
nathan <nathan.consolo17@gmail.com>
2024-06-07 14:47:33 -0300

    Validacao de criar conta finalizada

--- js/criar-conta.js
+++ js/criar-conta.js
@@ -1,16 +1,19 @@
 let criarUser = { nome: "", emailUsuario: "", senhaUsuario: "", cpf: 0 };
 
+
+
 const criarConta = (nomeU, cpfU, emailU, senhaU) => {
-  let msg = document.querySelector(".valida-cc");
+  
+  let msgCC = document.querySelector(".valida-cc");
 
   if (!nomeU.value || !cpfU.value || !emailU.value || !senhaU.value) {
-    msg.setAttribute("class", "negado");
-    msg.textContent = "Por favor, preencha todos os campos.";
-
+    msgCC.setAttribute("class", "negado");
+    msgCC.textContent = "Por favor, preencha todos os campos obrigatóros";
+    
     setTimeout(() => {
-      msg.setAttribute("class", "valida-cc");
-      msg.textContent = "";
-    }, 5000);
+      msgCC.setAttribute("class", "valida-cc");
+      msgCC.textContent = "";
+    }, 4000);
 
     return false;
   }
